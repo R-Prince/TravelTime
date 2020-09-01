1767b3c17e29bac0ac79abccc37e8b2c10e9b2e9
R-Prince <reneprince93@icloud.com>
2020-09-01 23:44:43 +0000

    Add emailJs SDK to contact form so the user enquiry is sent to website owners email address

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,11 @@
+function sendMail(contactForm){
+    
+    emailjs.send("gmail","rosieucd",{
+        "customer_enquiry": contactForm.text.value,
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "from_number": contactForm.number.value
+    });
+
+    return false;
+}
